24a0bcaaa9959db4483509484890a70af1ea21d4
RedSuns Chan <redsunschan@gmail.com>
2022-09-04 21:30:27 +0800

    update workexp and reduce bg img weight

--- src/sections/WorkExp/WorkExp.jsx
+++ src/sections/WorkExp/WorkExp.jsx
@@ -4,10 +4,7 @@ import './WorkExp.css'
 export default function WorkExp() {
     return (
         <div className="work-exp">
-            <h1>
-                Work
-                Experiences
-            </h1>
+            <h1>Work <div className='title-keyword'>Experiences</div></h1>
         </div>
     )
 }
