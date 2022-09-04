70db123c1980adc67f1bf3e8c701998e4f1de83e
RedSuns Chan <redsunschan@gmail.com>
2022-09-05 01:20:49 +0800

    optimize style of component and intro page

--- src/sections/Intro/Intro.jsx
+++ src/sections/Intro/Intro.jsx
@@ -3,6 +3,7 @@ import "./Intro.css";
 import SkillTag from "../../components/SkillTag/SkillTag";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import SocialIcon from "../../components/SocialIcon/SocialIcon";
 
 export default function Intro({fullpageApi}) {
     return (
@@ -31,10 +32,10 @@ export default function Intro({fullpageApi}) {
             </div>
 
             <div className="social-links">
-                <a className='linkedin' target='_blank' rel='noreferrer noopener' href="https://www.linkedin.com/in/redsuns-chan/"><FontAwesomeIcon icon={brands('linkedin')} /></a>
-                <a className='github' target='_blank' rel='noreferrer noopener' href="https://github.com/redsuns-chan"><FontAwesomeIcon icon={brands('github-square')} /></a>
-                <a className='twitter' target='_blank' rel='noreferrer noopener' href="https://twitter.com/redsunschan"><FontAwesomeIcon icon={brands('twitter-square')} /></a>
-                <a className='medium' target='_blank' rel='noreferrer noopener' href="https://medium.com/@redsunschan"><FontAwesomeIcon icon={brands('medium')} /></a>
+				<SocialIcon className='icon-linkedin' icon={brands('linkedin')} link='https://www.linkedin.com/in/redsuns-chan/'/>
+				<SocialIcon className='icon-github' icon={brands('github-square')} link='https://github.com/redsuns-chan'/>
+				<SocialIcon className='icon-twitter' icon={brands('twitter-square')} link='https://twitter.com/redsunschan'/>
+				<SocialIcon className='icon-medium' icon={brands('medium')} link='https://medium.com/@redsunschan'/>
             </div>
         </div>
     );
