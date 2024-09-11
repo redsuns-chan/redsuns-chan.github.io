49c3f860aac21de7ddc968e58ee073092b5cba6b
RedSuns Chan <redsunschan@gmail.com>
2024-09-12 03:27:19 +0800

    New Branch for 2024 Design

--- src/index.js
+++ src/index.js
@@ -1,33 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ReactFullpage from '@fullpage/react-fullpage';
 
-import reactFullpageKey from './react-fullpage-key';
-import Intro from './sections/Intro/Intro';
-import WorkExp from './sections/WorkExp/WorkExp';
-import Skills from './sections/Skills/Skills';
+import { App } from 'antd';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ReactFullpage
-	licenseKey={reactFullpageKey}
-	scrollingSpeed={800}
-	anchors={['workexp', 'projects', 'about', 'contact']}
-
-	render={({ state, fullpageApi }) => {
-		return (<ReactFullpage.Wrapper>
-			<div className='section' id='section-intro' data-anchor='intro'>
-				<Intro fullpageApi={fullpageApi}></Intro>
-			</div>
-			<div className='section' id='section-skills' data-anchor='skills'>
-				<Skills fullpageApi={fullpageApi}></Skills>
-			</div>
-			<div className='section' id='section-workexp' data-anchor='workexp'>
-				<WorkExp fullpageApi={fullpageApi}></WorkExp>
-			</div>
-		</ReactFullpage.Wrapper>)
-	}}
-/>);
+root.render(<App>
+	<h1>Welcome to Version 2024</h1>
+</App>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
